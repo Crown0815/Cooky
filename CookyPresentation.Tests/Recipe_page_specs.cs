082eedaafbc53ad6297021a5ad9aff46a5f837f4
Felix Kröner <felix.kroener@gmx.de>
2023-01-06 16:49:21 +0100

    refactor: Clean specs

--- CookyPresentation.Tests/Recipe_page_specs.cs
+++ CookyPresentation.Tests/Recipe_page_specs.cs
@@ -7,30 +7,26 @@ namespace CookyPresentation.Tests;
 
 public class Recipe_page_specs
 {
+    private static readonly Recipe Recipe = Recipe.New();
+
     [Fact]
     public void A_recipe_when_its_title_is_changed_has_the_given_title()
     {
-        var r = Recipe.New();
-        r.Title = GivenTitle;
-
-        r.Title.Should().Be(GivenTitle);
+        Recipe.Title = GivenTitle;
+        Recipe.Title.Should().Be(GivenTitle);
     }
     
     [Fact]
     public void A_recipe_when_its_text_is_changed_has_the_given_text()
     {
-        var r = Recipe.New();
-        r.Text = GivenText;
-
-        r.Text.Should().Be(GivenText);
+        Recipe.Text = GivenText;
+        Recipe.Text.Should().Be(GivenText);
     }
     
     [Fact]
     public void A_recipe_when_its_text_is_changed_to_text_with_surrounding_blank_lines_has_the_trimmed_text()
     {
-        var r = Recipe.New();
-        r.Text = GivenTextWithSurroundingBlankLines;
-
-        r.Text.Should().Be(GivenText);
+        Recipe.Text = GivenTextWithSurroundingBlankLines;
+        Recipe.Text.Should().Be(GivenText);
     }
 }
--- CookyPresentation.Tests/Recipe_persistence_specs.cs
+++ CookyPresentation.Tests/Recipe_persistence_specs.cs
@@ -6,27 +6,26 @@ namespace CookyPresentation.Tests;
 
 public class Recipe_persistence_specs
 {
+    private static readonly Recipe Recipe = Recipe.New();
+
+    private static Recipe SavedAndLoaded(Recipe recipe)
+    {
+        recipe.SaveCommand.Execute(recipe);
+        var rLoaded = Recipe.Load(recipe.Filename);
+        return rLoaded;
+    }
+
     [Fact]
     public void A_recipe_when_saved_and_loaded_preserves_its_given_title()
     {
-        var r = Recipe.New();
-        r.Title = Example.GivenTitle;
-        
-        r.SaveCommand.Execute(r);
-        var rLoaded = Recipe.Load(r.Filename);
-
-        rLoaded.Title.Should().Be(Example.GivenTitle);
+        Recipe.Title = Example.GivenTitle;
+        SavedAndLoaded(Recipe).Title.Should().Be(Example.GivenTitle);
     }
-    
+
     [Fact]
     public void A_recipe_when_saved_and_loaded_preserves_its_given_text()
     {
-        var r = Recipe.New();
-        r.Text = Example.GivenText;
-        
-        r.SaveCommand.Execute(r);
-        var rLoaded = Recipe.Load(r.Filename);
-
-        rLoaded.Text.Should().Be(Example.GivenText);
+        Recipe.Text = Example.GivenText;
+        SavedAndLoaded(Recipe).Text.Should().Be(Example.GivenText);
     }
 }
