b07136312950ce91e7e5fe01ebea0397d2cdbd1b
Felix Kröner <felix.kroener@gmx.de>
2023-01-19 20:52:24 +0100

    test: Transfer tests to run directly on RecipePage rather than through commands

--- CookyPresentation.Tests/Recipe_persistence_specs.cs
+++ CookyPresentation.Tests/Recipe_persistence_specs.cs
@@ -6,7 +6,7 @@ namespace CookyPresentation.Tests;
 
 public class Recipe_persistence_specs
 {
-    private static readonly RecipePage Recipe = RecipePage.New();
+    private readonly RecipePage _recipe = RecipePage.New();
 
     private static RecipePage SavedAndLoaded(RecipePage recipe)
     {
@@ -15,36 +15,36 @@ public class Recipe_persistence_specs
     }
 
     private static void Save(RecipePage recipe) => 
-        RecipeCommands.SaveCommand.Execute(recipe);
+        recipe.Save().GetAwaiter().GetResult();
 
     private static RecipePage Load(RecipePage recipe) => 
         RecipePage.Load(recipe.Filename);
 
     private static void Delete(RecipePage recipe) =>
-        RecipeCommands.DeleteCommand.Execute(recipe);
+        recipe.Delete();
 
     [Fact]
     public void A_recipe_when_saved_and_loaded_preserves_its_given_title()
     {
-        Recipe.Title = Example.GivenTitle;
-        SavedAndLoaded(Recipe).Title.Should().Be(Example.GivenTitle);
+        _recipe.Title = Example.GivenTitle;
+        SavedAndLoaded(_recipe).Title.Should().Be(Example.GivenTitle);
     }
 
     [Fact]
     public void A_recipe_when_saved_and_loaded_preserves_its_given_instructions()
     {
-        Recipe.Instructions = Example.Instructions;
-        SavedAndLoaded(Recipe).Instructions.Should().Be(Example.Instructions);
+        _recipe.Instructions = Example.Instructions;
+        SavedAndLoaded(_recipe).Instructions.Should().Be(Example.Instructions);
     }
 
     [Fact]
     public void A_recipe_when_saved_and_deleted_cannot_be_loaded()
     {
-        Save(Recipe);
-        Delete(Recipe);
+        Save(_recipe);
+        Delete(_recipe);
         
-        FluentActions.Invoking(() => RecipePage.Load(Recipe.Filename))
+        FluentActions.Invoking(() => RecipePage.Load(_recipe.Filename))
             .Should().Throw<RecipeNotFoundException>()
-            .WithMessage($"*'{Recipe.Filename}' was not found*");
+            .WithMessage($"*'{_recipe.Filename}' was not found*");
     }
 }
