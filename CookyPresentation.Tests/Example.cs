48d3b5cc2c49b2effd537ec1132bd6ac0e5b68f4
Felix Kröner <felix.kroener@gmx.de>
2023-02-04 12:53:37 +0100

    refactor: Align strings with tab stops
    They were off by one space before

--- CookyPresentation.Tests/Example.cs
+++ CookyPresentation.Tests/Example.cs
@@ -5,26 +5,26 @@ internal static class Example
     public const string GivenTitle = "new title";
 
     public const string Instructions = """
-                                     Some text with a
-                                     linebreak
-                                     """;
+                                    Some text with a
+                                    linebreak
+                                    """;
 
     public const string GivenTextWithSurroundingBlankLines = $"""
 
-                                     {Instructions}
+                                    {Instructions}
 
-                                     """;
+                                    """;
 
     public const string TrimmedIngredientsText = """
-                                   Carrot
-                                   Meat
-                                   """;
+                                    Carrot
+                                    Meat
+                                    """;
     
     public const string IngredientsWithBlankLinesAndTrailingWhitespace = """
 
-                                      Carrot
+                                       Carrot
 
-                                   Meat   
+                                    Meat   
 
-                                   """;
+                                    """;
 }
